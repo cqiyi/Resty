24c33a638915d8efdd8270faf55f600d7be499c4
cqiyi <cqiyi@hotmail.com>
2015-09-25 03:52:56 +0800

    resty-security module with perfect

--- resty-security/src/main/java/cn/dreampie/security/SessionBuilder.java
+++ resty-security/src/main/java/cn/dreampie/security/SessionBuilder.java
@@ -1,6 +1,5 @@
 package cn.dreampie.security;
 
-
 import cn.dreampie.common.http.HttpRequest;
 import cn.dreampie.common.http.HttpResponse;
 import cn.dreampie.common.util.Maper;
@@ -17,183 +16,219 @@ import java.util.Map;
  */
 public class SessionBuilder {
 
-  private final static Logger logger = Logger.getLogger(SessionBuilder.class);
-  private final static String ANONYMOUS = "anonymous";
-  private final Sessions sessions;
-  private final Signer signer;
-  private final SessionCookieDescriptor sessionCookieDescriptor;
-  private final Session emptySession;
+	private final static Logger logger = Logger.getLogger(SessionBuilder.class);
+	private final static String ANONYMOUS = "anonymous";
+	private final Sessions sessions;
+	private final Signer signer;
+	private final SessionCookieDescriptor sessionCookieDescriptor;
+	private final Session emptySession;
 
-  public SessionBuilder(long defaultExpires, int limit, int rememberDay, AuthenticateService authenticateService) {
-    this.sessions = new Sessions(defaultExpires, limit);
-    this.signer = new CookieSigner();
-    this.sessionCookieDescriptor = new SessionCookieDescriptor();
-    this.emptySession = new Session();
+	public SessionBuilder(long defaultExpires, int limit, int rememberDay,
+			AuthenticateService authenticateService) {
+		this.sessions = new Sessions(defaultExpires, limit);
+		this.signer = new CookieSigner();
+		this.sessionCookieDescriptor = new SessionCookieDescriptor();
+		this.emptySession = new Session();
 
-    Subject.init(rememberDay, new Credentials(authenticateService, defaultExpires), authenticateService.getPasswordService());
-  }
+		Subject.init(rememberDay, new Credentials(authenticateService,
+				defaultExpires), authenticateService.getPasswordService());
+	}
 
-  /**
-   * 从cookie读取用户信息
-   *
-   * @param request
-   * @return
-   */
-  public Session in(HttpRequest request) {
-    Session session = buildSession(request);
-    Subject.updateCurrent(session);
-    return session;
-  }
+	/**
+	 * 从cookie读取用户信息
+	 *
+	 * @param request
+	 * @return
+	 */
+	public Session in(HttpRequest request) {
+		// cqiyi@hotmail.com 2015年9月21日07:45:01
+		String apikey = request.getHeader("apikey");
+		if (apikey == null || "".equals(apikey.trim())) {
+			return buildSession(request);
+		} else {
+			return buildSessionUsingApikey(request);
+		}
+	}
 
-  /**
-   * 写出到cookie
-   *
-   * @param oldSession
-   * @param response
-   */
-  public Session out(Session oldSession, HttpResponse response) {
-    Session session = Subject.current();
-    String username = null;
-    String sessionKey = session.getSessionKey();
-    Principal principal = session.getPrincipal();
+	private Session buildSessionUsingApikey(HttpRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    if (principal != null) {
-      username = principal.getUsername();
-    } else {
-      username = getAnonymousName(session);
-    }
-    if (session != oldSession) {
-      //更新cookie
-      updateCookie(response, session);
-      //重新存储session数据
-      String oldSessionKey = oldSession.getSessionKey();
-      String oldUsername = null;
-      Principal oldPrincipal = oldSession.getPrincipal();
-      //原本的session
-      if (oldPrincipal != null) {
-        oldUsername = oldPrincipal.getUsername();
-      } else {
-        oldUsername = getAnonymousName(oldSession);
-      }
-      //现在的session
-      sessions.update(oldUsername, oldSessionKey, username, sessionKey, session);
-    } else {
-      sessions.update(username, sessionKey, session);
-    }
-    return session;
-  }
+	/**
+	 * 写出到cookie
+	 *
+	 * @param oldSession
+	 * @param response
+	 */
+	public Session out(Session oldSession, HttpResponse response) {
+		// TODO cqiyi：如果request的header中有apikey，则不通过cookies生成，直接退出
+		Session session = Subject.current();
+		String username = null;
+		String sessionKey = session.getSessionKey();
+		Principal principal = session.getPrincipal();
 
-  /**
-   * 构建session数据
-   *
-   * @param request
-   * @return
-   */
-  private Session getSession(HttpRequest request, Session session) {
-    String agent = request.getHeader("User-Agent");
-    session.set(Maper.of(Sessions.ADDRESS_KEY, request.getClientAddress(),
-        Sessions.AGENT_KEY, agent == null ? "Unknown" : agent));
-    return session;
-  }
+		if (principal != null) {
+			username = principal.getUsername();
+		} else {
+			username = getAnonymousName(session);
+		}
+		if (session != oldSession) {
+			// 更新cookie
+			updateCookie(response, session);
+			// 重新存储session数据
+			String oldSessionKey = oldSession.getSessionKey();
+			String oldUsername = null;
+			Principal oldPrincipal = oldSession.getPrincipal();
+			// 原本的session
+			if (oldPrincipal != null) {
+				oldUsername = oldPrincipal.getUsername();
+			} else {
+				oldUsername = getAnonymousName(oldSession);
+			}
+			// 现在的session
+			sessions.update(oldUsername, oldSessionKey, username, sessionKey,
+					session);
+		} else {
+			sessions.update(username, sessionKey, session);
+		}
+		return session;
+	}
 
-  /**
-   * 获取匿名用户的名字
-   *
-   * @param session
-   * @return
-   */
-  private String getAnonymousName(Session session) {
-    return ANONYMOUS + "@" + session.get(Sessions.ADDRESS_KEY);
-  }
+	/**
+	 * 构建session数据
+	 *
+	 * @param request
+	 * @return
+	 */
+	private Session getSession(HttpRequest request, Session session) {
+		String agent = request.getHeader("User-Agent");
+		session.set(Maper.of(Sessions.ADDRESS_KEY, request.getClientAddress(),
+				Sessions.AGENT_KEY, agent == null ? "Unknown" : agent));
+		return session;
+	}
 
-  /**
-   * 初始化session
-   *
-   * @param request
-   * @return
-   */
-  private Session buildSession(HttpRequest request) {
-    String sessionCookieName = sessionCookieDescriptor.getCookieName();
-    String cookie = request.getCookieValue(sessionCookieName);
-    if (cookie != null) {
-      logger.debug("Session cookie was: %s.", cookie);
-      String sig = request.getCookieValue(sessionCookieDescriptor.getCookieSignatureName());
-      logger.debug("Session signature was: %s.", sig);
-      if (sig != null && signer.verify(cookie, sig)) {
-        Map<String, String> cookies = readCookies(cookie);
-        //失效时间
-        Map<String, String> cookieValues = Maper.copyOf(cookies);
-        String principalName = cookieValues.get(Principal.PRINCIPAL_DEF_KEY);
-        if (principalName != null && !"".equals(principalName.trim())) {
-          //判断 是否使用了 remeberme 或失效
-          Sessions.SessionDatas sessionDatas = sessions.get(principalName);
-          if (sessionDatas != null) {
-            String sessionKey = cookieValues.get(Session.SESSION_DEF_KEY);
-            Sessions.SessionData sessionData = sessionDatas.getSessionData(sessionKey);
-            if (sessionData != null) {
-              logger.debug("Found session success. username was: %s.", principalName);
-              return sessionData.getSession();
-            }
-          } else {
-            logger.warn("Invalid user session. session was: %s. Ignoring session cookie.", cookie);
-          }
-        } else {
-          logger.warn("Invalid username. session was: %s. Ignoring session cookie.", cookie);
-        }
-      } else {
-        logger.warn("Invalid session signature. session was: %s. Ignoring session cookie.", cookie);
-      }
-    }
-    return getSession(request, emptySession);
-  }
+	/**
+	 * 获取匿名用户的名字
+	 *
+	 * @param session
+	 * @return
+	 */
+	private String getAnonymousName(Session session) {
+		return ANONYMOUS + "@" + session.get(Sessions.ADDRESS_KEY);
+	}
 
-  /**
-   * 转换数据cookie
-   *
-   * @param cookie
-   * @return
-   */
-  private Map<String, String> readCookies(String cookie) {
-    return (Map<String, String>) Jsoner.toObject(cookie, Map.class);
-  }
+	/**
+	 * 初始化session
+	 *
+	 * @param request
+	 * @return
+	 */
+	private Session buildSession(HttpRequest request) {
+		// TODO cqiyi：如果request的header中有apikey，则不通过cookies生成
+		String sessionCookieName = sessionCookieDescriptor.getCookieName();
+		String cookie = request.getCookieValue(sessionCookieName);
+		if (cookie != null) {
+			logger.debug("Session cookie was: %s.", cookie);
+			String sig = request.getCookieValue(sessionCookieDescriptor
+					.getCookieSignatureName());
+			logger.debug("Session signature was: %s.", sig);
+			if (sig != null && signer.verify(cookie, sig)) {
+				Map<String, String> cookies = readCookies(cookie);
+				// 失效时间
+				Map<String, String> cookieValues = Maper.copyOf(cookies);
+				String principalName = cookieValues
+						.get(Principal.PRINCIPAL_DEF_KEY);
+				if (principalName != null && !"".equals(principalName.trim())) {
+					// 判断 是否使用了 remeberme 或失效
+					Sessions.SessionDatas sessionDatas = sessions
+							.get(principalName);
+					if (sessionDatas != null) {
+						String sessionKey = cookieValues
+								.get(Session.SESSION_DEF_KEY);
+						Sessions.SessionData sessionData = sessionDatas
+								.getSessionData(sessionKey);
+						if (sessionData != null) {
+							logger.debug(
+									"Found session success. username was: %s.",
+									principalName);
+							return sessionData.getSession();
+						}
+					} else {
+						logger.warn(
+								"Invalid user session. session was: %s. Ignoring session cookie.",
+								cookie);
+					}
+				} else {
+					logger.warn(
+							"Invalid username. session was: %s. Ignoring session cookie.",
+							cookie);
+				}
+			} else {
+				logger.warn(
+						"Invalid session signature. session was: %s. Ignoring session cookie.",
+						cookie);
+			}
+		}
+		return getSession(request, emptySession);
+	}
 
-  /**
-   * 写入cookie到客户端
-   *
-   * @param resp
-   * @param session
-   */
-  private void updateCookie(HttpResponse resp, Session session) {
-    Map<String, String> cookiesMap = getCookiesMap(session);
-    if (cookiesMap.isEmpty()) {
-      resp.clearCookie(sessionCookieDescriptor.getCookieName());
-      resp.clearCookie(sessionCookieDescriptor.getCookieSignatureName());
-    } else {
-      for (Map.Entry<String, String> cookie : cookiesMap.entrySet()) {
-        resp.addCookie(cookie.getKey(), cookie.getValue(), session.getExpires() > 0 ? (int) (session.getExpires() / 1000) : (int) session.getExpires());
-      }
-    }
-  }
+	/**
+	 * 转换数据cookie
+	 *
+	 * @param cookie
+	 * @return
+	 */
+	private Map<String, String> readCookies(String cookie) {
+		return (Map<String, String>) Jsoner.toObject(cookie, Map.class);
+	}
 
-  /**
-   * session 转换为cookie
-   *
-   * @param session
-   * @return
-   */
-  private Map<String, String> getCookiesMap(Session session) {
+	/**
+	 * 写入cookie到客户端
+	 *
+	 * @param resp
+	 * @param session
+	 */
+	private void updateCookie(HttpResponse resp, Session session) {
+		Map<String, String> cookiesMap = getCookiesMap(session);
+		if (cookiesMap.isEmpty()) {
+			resp.clearCookie(sessionCookieDescriptor.getCookieName());
+			resp.clearCookie(sessionCookieDescriptor.getCookieSignatureName());
+		} else {
+			for (Map.Entry<String, String> cookie : cookiesMap.entrySet()) {
+				resp.addCookie(cookie.getKey(), cookie.getValue(), session
+						.getExpires() > 0 ? (int) (session.getExpires() / 1000)
+						: (int) session.getExpires());
+			}
+		}
+	}
 
-    Map<String, String> sessionMap = Maper.of(Session.SESSION_DEF_KEY, session.getSessionKey());
-    Principal principal = session.getPrincipal();
-    if (principal != null) {
-      sessionMap.put(Principal.PRINCIPAL_DEF_KEY, principal.getUsername());
-    } else {
-      sessionMap.put(Principal.PRINCIPAL_DEF_KEY, getAnonymousName(session));
-    }
-    String sessionJson = Jsoner.toJSON(sessionMap);
-    String sessionJsonSign = signer.sign(sessionJson);
-    logger.info("Save cookie " + sessionCookieDescriptor.getCookieName() + ": " + sessionJson + ", " + sessionCookieDescriptor.getCookieSignatureName() + ": " + sessionJsonSign);
-    return Maper.of(sessionCookieDescriptor.getCookieName(), sessionJson,
-        sessionCookieDescriptor.getCookieSignatureName(), sessionJsonSign);
-  }
+	/**
+	 * session 转换为cookie
+	 *
+	 * @param session
+	 * @return
+	 */
+	private Map<String, String> getCookiesMap(Session session) {
+
+		Map<String, String> sessionMap = Maper.of(Session.SESSION_DEF_KEY,
+				session.getSessionKey());
+		Principal principal = session.getPrincipal();
+		if (principal != null) {
+			sessionMap
+					.put(Principal.PRINCIPAL_DEF_KEY, principal.getUsername());
+		} else {
+			sessionMap.put(Principal.PRINCIPAL_DEF_KEY,
+					getAnonymousName(session));
+		}
+		String sessionJson = Jsoner.toJSON(sessionMap);
+		String sessionJsonSign = signer.sign(sessionJson);
+		logger.info("Save cookie " + sessionCookieDescriptor.getCookieName()
+				+ ": " + sessionJson + ", "
+				+ sessionCookieDescriptor.getCookieSignatureName() + ": "
+				+ sessionJsonSign);
+		return Maper.of(sessionCookieDescriptor.getCookieName(), sessionJson,
+				sessionCookieDescriptor.getCookieSignatureName(),
+				sessionJsonSign);
+	}
 }
--- resty-security/src/main/java/cn/dreampie/security/Subject.java
+++ resty-security/src/main/java/cn/dreampie/security/Subject.java
@@ -1,241 +1,280 @@
 package cn.dreampie.security;
 
+import static cn.dreampie.common.util.Checker.checkNotNull;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
 import cn.dreampie.common.http.exception.WebException;
 import cn.dreampie.common.http.result.HttpStatus;
 import cn.dreampie.common.util.matcher.AntPathMatcher;
 import cn.dreampie.log.Logger;
 import cn.dreampie.security.credential.Credential;
 import cn.dreampie.security.credential.Credentials;
-
-import java.util.Calendar;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-
-import static cn.dreampie.common.util.Checker.checkNotNull;
+import cn.dreampie.security.encode.RandomString;
+import cn.dreampie.security.sign.Token;
 
 /**
  * Created by wangrenhui on 14/12/23.
  */
 public class Subject {
-  private static final Logger logger = Logger.getLogger(Subject.class);
+	private static final Logger logger = Logger.getLogger(Subject.class);
 
-  private static final ThreadLocal<Session> sessionTL = new ThreadLocal<Session>();
-  private static Credentials credentials;
-  private static PasswordService passwordService;
-  private static int rememberDay;
+	private static final ThreadLocal<Session> sessionTL = new ThreadLocal<Session>();
+	private static Credentials credentials;
+	private static PasswordService passwordService;
+	private static int rememberDay;
 
-  static void init(int rememberDay, Credentials credentials, PasswordService passwordService) {
-    Subject.rememberDay = rememberDay;
-    Subject.credentials = credentials;
-    Subject.passwordService = passwordService;
-  }
+	static void init(int rememberDay, Credentials credentials,
+			PasswordService passwordService) {
+		Subject.rememberDay = rememberDay;
+		Subject.credentials = credentials;
+		Subject.passwordService = passwordService;
+	}
 
-  static Session current() {
-    return sessionTL.get();
-  }
+	static Session current() {
+		return sessionTL.get();
+	}
 
-  static Session updateCurrent(Session session) {
-    if (session != current()) {
-      sessionTL.set(session);
-    }
-    return session;
-  }
+	static Session updateCurrent(Session session) {
+		if (session != current()) {
+			sessionTL.set(session);
+		}
+		return session;
+	}
 
-  private static void removeCurrent() {
-    sessionTL.remove();
-  }
+	private static void removeCurrent() {
+		sessionTL.remove();
+	}
 
-  private static Session authenticateAs(Principal principal, long expires) {
-    String sessionKey = UUID.randomUUID().toString();
-    return updateCurrent(new Session(sessionKey, principal, current().getValues(), expires));
-  }
+	private static Session authenticateAs(Principal principal, long expires) {
+		String sessionKey = UUID.randomUUID().toString();
+		return updateCurrent(new Session(sessionKey, principal, current()
+				.getValues(), expires));
+	}
 
-  private static Session clearPrincipal() {
-    Session session = current();
-    Principal principal = session.getPrincipal();
-    if (principal != null) {
-      credentials.removePrincipal(principal.getUsername());
-    }
-    return updateCurrent(new Session());
-  }
+	private static Session clearPrincipal() {
+		Session session = current();
+		Principal principal = session.getPrincipal();
+		if (principal != null) {
+			credentials.removePrincipal(principal.getUsername());
+		}
+		return updateCurrent(new Session());
+	}
 
-  /**
-   * login user
-   *
-   * @param username
-   * @param password
-   * @param rememberMe
-   * @return
-   */
-  public static void login(String username, String password, boolean rememberMe) {
-    checkNotNull(username, "Username could not be null.");
-    checkNotNull(password, "Password could not be null.");
-    Principal principal = credentials.getPrincipal(username);
-    if (principal == null) {
-      throw new WebException(HttpStatus.NOT_FOUND, "User not found.");
-    }
-    boolean match = false;
-    String salt = principal.getSalt();
-    if (salt != null && !salt.isEmpty()) {
-      match = passwordService.match(password, principal.getPasswordHash(), salt);
-    } else {
-      match = passwordService.match(password, principal.getPasswordHash());
-    }
+	/**
+	 * login user
+	 *
+	 * @param username
+	 * @param password
+	 * @param rememberMe
+	 * @return
+	 */
+	public static void login(String username, String password,
+			boolean rememberMe) {
+		checkNotNull(username, "Username could not be null.");
+		checkNotNull(password, "Password could not be null.");
+		Principal principal = credentials.getPrincipal(username);
+		if (principal == null) {
+			throw new WebException(HttpStatus.NOT_FOUND, "User not found.");
+		}
+		boolean match = false;
+		String salt = principal.getSalt();
+		if (salt != null && !salt.isEmpty()) {
+			match = passwordService.match(password,
+					principal.getPasswordHash(), salt);
+		} else {
+			match = passwordService
+					.match(password, principal.getPasswordHash());
+		}
 
-    if (match) {
-      //授权用户
-      //时间
-      long expires = -1;
-      if (rememberMe) {
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, rememberDay);
-        expires = cal.getTimeInMillis();
-      }
-      //授权用户
-      authenticateAs(principal, expires);
-      logger.info("Session authentication as " + username);
-    } else {
-      throw new WebException(HttpStatus.UNPROCESSABLE_ENTITY, "Password not match.");
-    }
-  }
+		if (match) {
+			// 授权用户
+			// 时间
+			long expires = -1;
+			if (rememberMe) {
+				Calendar cal = Calendar.getInstance();
+				cal.add(Calendar.DATE, rememberDay);
+				expires = cal.getTimeInMillis();
+			}
+			// 授权用户
+			authenticateAs(principal, expires);
+			logger.info("Session authentication as " + username);
+		} else {
+			throw new WebException(HttpStatus.UNPROCESSABLE_ENTITY,
+					"Password not match.");
+		}
+	}
 
-  public static void logout() {
-    //add cache
-    Principal principal = current().getPrincipal();
-    if (principal != null) {
-      logger.info("Session leave authentication " + principal.getUsername());
-    }
-    //清理用户
-    clearPrincipal();
-  }
+	public static void logout() {
+		// add cache
+		Principal principal = current().getPrincipal();
+		if (principal != null) {
+			logger.info("Session leave authentication "
+					+ principal.getUsername());
+		}
+		// 清理用户
+		clearPrincipal();
+	}
+
+	public static void login(String username, String password) {
+		login(username, password, false);
+	}
+
+	public static long getExpires() {
+		return current().getExpires();
+	}
+
+	public static Principal getPrincipal() {
+		return current().getPrincipal();
+	}
+
+	public static Map<String, String> getValues() {
+		return current().getValues();
+	}
+
+	public static String get(String key) {
+		return current().get(key);
+	}
+
+	public static void set(String key, String value) {
+		current().set(key, value);
+	}
+
+	public static String remove(String key) {
+		return current().remove(key);
+	}
+
+	/**
+	 * 当前api需要的权限值
+	 *
+	 * @param httpMethod
+	 *            httpMethod
+	 * @param path
+	 *            path
+	 * @return value
+	 */
+	public static String need(String httpMethod, String path) {
+		Map<String, Map<String, Set<Credential>>> credentialMap = credentials
+				.getAllCredentials();
+
+		String value;
+		if (credentialMap.containsKey(httpMethod)) {
+			// 匹配method的map
+			value = matchPath(httpMethod, path, credentialMap);
+			if (value == null) {
+				value = matchPath("*", path, credentialMap);
+			}
+		} else {
+			value = matchPath("*", path, credentialMap);
+		}
+		return value;
+	}
+
+	/**
+	 * 匹配规则，优先httpMethod，其次相同的起始位置
+	 *
+	 * @param httpMethod
+	 *            httpMethod
+	 * @param path
+	 *            path
+	 * @param credentialMap
+	 *            credentialMap
+	 * @return value
+	 */
+	private static String matchPath(String httpMethod, String path,
+			Map<String, Map<String, Set<Credential>>> credentialMap) {
+		if (credentialMap != null && credentialMap.size() > 0) {
+			Map<String, Set<Credential>> credentials = credentialMap
+					.get(httpMethod);
+			if (credentials != null && credentials.size() > 0) {
+				Set<Map.Entry<String, Set<Credential>>> credentialsEntry = credentials
+						.entrySet();
+				Set<Credential> credentialSet;
+				for (Map.Entry<String, Set<Credential>> credentialEntry : credentialsEntry) {
+					if (path.startsWith(credentialEntry.getKey())) {
+						credentialSet = credentialEntry.getValue();
+						for (Credential credential : credentialSet) {
+							if (AntPathMatcher.instance().match(
+									credential.getAntPath(), path)) {
+								return credential.getValue();
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 检测权限
+	 *
+	 * @param httpMethod
+	 *            httpMethod
+	 * @param path
+	 *            path
+	 */
+	public static void check(String httpMethod, String path) {
+		// TODO 实现黑名单，白名单的功能
+		String needCredential = need(httpMethod, path);
+		logger.info(httpMethod + " " + path + " need credential "
+				+ needCredential);
+		if (needCredential != null) {
+			Principal principal = current().getPrincipal();
+			if (principal != null) {
+				if (!principal.hasCredential(needCredential)) {
+					throw new WebException(HttpStatus.FORBIDDEN);
+				}
+			} else {
+				throw new WebException(HttpStatus.UNAUTHORIZED);
+			}
+		}
+	}
+
+	/**
+	 * 判断是否有当前api权限
+	 *
+	 * @param httpMethod
+	 *            httpMethod
+	 * @param path
+	 *            path
+	 * @return boolean
+	 */
+	public static boolean has(String httpMethod, String path) {
+		String needCredential = need(httpMethod, path);
+		if (needCredential != null) {
+			Principal principal = current().getPrincipal();
+			if (principal != null) {
+				if (principal.hasCredential(needCredential)) {
+					return true;
+				}
+			}
+		} else {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 生成客户端认证的apikey和secretkey
+	 * 每次
+	 * @return
+	 */
+//	public static Map<String, String> generateClientKey() {
+//		// TODO cqiyi:随机生成apikey和secretkey
+//		// map.put(key, value);
+//		Token token = Token.create();
+//		Map<String, String> map = new HashMap<String, String>();
+//		map.put(key, value)
+//	}
 
 
-  public static void login(String username, String password) {
-    login(username, password, false);
-  }
-
-  public static long getExpires() {
-    return current().getExpires();
-  }
-
-  public static Principal getPrincipal() {
-    return current().getPrincipal();
-  }
-
-  public static Map<String, String> getValues() {
-    return current().getValues();
-  }
-
-  public static String get(String key) {
-    return current().get(key);
-  }
-
-  public static void set(String key, String value) {
-    current().set(key, value);
-  }
-
-  public static String remove(String key) {
-    return current().remove(key);
-  }
-
-  /**
-   * 当前api需要的权限值
-   *
-   * @param httpMethod httpMethod
-   * @param path       path
-   * @return value
-   */
-  public static String need(String httpMethod, String path) {
-    Map<String, Map<String, Set<Credential>>> credentialMap = credentials.getAllCredentials();
-
-    String value;
-    if (credentialMap.containsKey(httpMethod)) {
-      //匹配method的map
-      value = matchPath(httpMethod, path, credentialMap);
-      if (value == null) {
-        value = matchPath("*", path, credentialMap);
-      }
-    } else {
-      value = matchPath("*", path, credentialMap);
-    }
-    return value;
-  }
-
-  /**
-   * 匹配规则，优先httpMethod，其次相同的起始位置
-   *
-   * @param httpMethod    httpMethod
-   * @param path          path
-   * @param credentialMap credentialMap
-   * @return value
-   */
-  private static String matchPath(String httpMethod, String path, Map<String, Map<String, Set<Credential>>> credentialMap) {
-    if (credentialMap != null && credentialMap.size() > 0) {
-      Map<String, Set<Credential>> credentials = credentialMap.get(httpMethod);
-      if (credentials.size() > 0) {
-        Set<Map.Entry<String, Set<Credential>>> credentialsEntry = credentials.entrySet();
-        Set<Credential> credentialSet;
-        for (Map.Entry<String, Set<Credential>> credentialEntry : credentialsEntry) {
-          if (path.startsWith(credentialEntry.getKey())) {
-            credentialSet = credentialEntry.getValue();
-            for (Credential credential : credentialSet) {
-              if (AntPathMatcher.instance().match(credential.getAntPath(), path)) {
-                return credential.getValue();
-              }
-            }
-          }
-        }
-      }
-    }
-    return null;
-  }
-
-  /**
-   * 检测权限
-   *
-   * @param httpMethod httpMethod
-   * @param path       path
-   */
-  public static void check(String httpMethod, String path) {
-    String needCredential = need(httpMethod, path);
-    logger.info(httpMethod + " " + path + " need credential " + needCredential);
-    if (needCredential != null) {
-      Principal principal = current().getPrincipal();
-      if (principal != null) {
-        if (!principal.hasCredential(needCredential)) {
-          throw new WebException(HttpStatus.FORBIDDEN);
-        }
-      } else {
-        throw new WebException(HttpStatus.UNAUTHORIZED);
-      }
-    }
-  }
-
-  /**
-   * 判断是否有当前api权限
-   *
-   * @param httpMethod httpMethod
-   * @param path       path
-   * @return boolean
-   */
-  public static boolean has(String httpMethod, String path) {
-    String needCredential = need(httpMethod, path);
-    if (needCredential != null) {
-      Principal principal = current().getPrincipal();
-      if (principal != null) {
-        if (principal.hasCredential(needCredential)) {
-          return true;
-        }
-      }
-    } else {
-      return true;
-    }
-    return false;
-  }
-
-  void set(Map<String, String> values) {
-    current().set(values);
-  }
+	void set(Map<String, String> values) {
+		current().set(values);
+	}
 
 }
